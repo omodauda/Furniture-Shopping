5dbbef8b9972aa2d971795adf4e10409e1255e28
omodauda <omodauda.dl@gmail.com>
2022-05-20 17:45:40 +0100

    auth custom header

---
+++ src/components/AuthCustomHeader.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {View, StyleSheet, Dimensions, Image} from 'react-native';
+import COLORS from '@constants/Colors';
+
+const {width} = Dimensions.get('screen');
+const LINE_WIDTH = 0.28 * width;
+const IMG_WIDTH = 0.17 * width;
+
+export default function AuthCustomHeader() {
+  return (
+    <View style={styles.header}>
+      <View style={styles.line} />
+      <Image
+        style={styles.logo}
+        source={require('@assets/images/header-logo.png')}
+      />
+      <View style={styles.line} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  line: {
+    borderRadius: 2,
+    backgroundColor: COLORS.gray4,
+    height: 1,
+    width: LINE_WIDTH,
+  },
+  logo: {
+    width: IMG_WIDTH,
+  },
+});
--- src/navigations/AppNavigator.js
+++ src/navigations/AppNavigator.js
@@ -8,7 +8,7 @@ const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
-    <Stack.Navigator headerMode="none">
+    <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Auth" component={AuthStack} />
     </Stack.Navigator>
   );
