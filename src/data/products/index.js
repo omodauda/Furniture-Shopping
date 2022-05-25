3c8024c1f886d70ecb0d58fe60d7ad0ccff628c2
omodauda <omodauda.dl@gmail.com>
2022-05-25 20:24:05 +0100

    products static data

---
+++ src/data/products/index.js
@@ -0,0 +1,201 @@
+const products = [
+  {
+    category: 'Chair',
+    list: [
+      {
+        id: '1',
+        name: 'Coffe Chair',
+        price: 20.0,
+        image: '',
+        units: 0,
+        rating: '5.0',
+        description:
+          'Coffee Chair is made of by natural wood.The design that is very simple and minimal. This is truly one of the best furnutures in any family for now. With 3 different colors, you can easily select the best match for your home.',
+        reviews: [
+          {
+            id: '1',
+            user: 'Busayo Elegbede',
+            rating: '5',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: 'Table',
+    list: [
+      {
+        id: '1',
+        name: 'Minimal Stand',
+        price: 25.0,
+        image: '',
+        units: 1,
+        rating: '4.5',
+        description:
+          'Minimal Stand is made of by natural wood.The design that is very simple and minimal. This is truly one of the best furnutures in any family for now. With 3 different colors, you can easily select the best match for your home.',
+        reviews: [
+          {
+            id: '1',
+            user: 'Bruno Fernandes',
+            rating: '5',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+          {
+            id: '2',
+            user: 'Tracy Mosby',
+            rating: '5',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+          {
+            id: '3',
+            user: 'Busayo Elegbede',
+            rating: '4',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+        ],
+      },
+      {
+        id: '2',
+        name: 'Simple Desk',
+        price: 50.0,
+        image: '',
+        units: 5,
+        rating: '4.3',
+        description:
+          'Simple Desk is made of by natural wood.The design that is very simple and minimal. This is truly one of the best furnutures in any family for now. With 3 different colors, you can easily select the best match for your home.',
+        reviews: [
+          {
+            id: '1',
+            user: 'Tracy Mosby',
+            rating: '5',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+          {
+            id: '2',
+            user: 'Busayo Elegbede',
+            rating: '4',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+        ],
+      },
+      {
+        id: '3',
+        name: 'Minimal Desk',
+        price: 50.0,
+        image: '',
+        units: 2,
+        rating: '4.8',
+        description:
+          'Minimal Desk is made of by natural wood.The design that is very simple and minimal. This is truly one of the best furnutures in any family for now. With 3 different colors, you can easily select the best match for your home.',
+        reviews: [
+          {
+            id: '1',
+            user: 'Bruno Fernandes',
+            rating: '5',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+          {
+            id: '2',
+            user: 'Busayo Elegbede',
+            rating: '4',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+        ],
+      },
+      {
+        id: '4',
+        name: 'Coffee Table',
+        price: 50.0,
+        image: '',
+        units: 6,
+        rating: '5.0',
+        description:
+          'Coffee Table is made of by natural wood.The design that is very simple and minimal. This is truly one of the best furnutures in any family for now. With 3 different colors, you can easily select the best match for your home.',
+        reviews: [
+          {
+            id: '1',
+            user: 'Busayo Elegbede',
+            rating: '4',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    category: 'Lamp',
+    list: [
+      {
+        id: '1',
+        name: 'Black Simple Lamp',
+        price: 12.0,
+        image: '',
+        units: 8,
+        rating: '4.7',
+        description:
+          'Black Simple Lamp is made of by natural wood.The design that is very simple and minimal. This is truly one of the best furnutures in any family for now. With 3 different colors, you can easily select the best match for your home.',
+        reviews: [
+          {
+            id: '1',
+            user: 'Bruno Fernandes',
+            rating: '5',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+          {
+            id: '2',
+            user: 'Busayo Elegbede',
+            rating: '4',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+        ],
+      },
+      {
+        id: '2',
+        name: 'Minimal Lamp',
+        price: 12.0,
+        image: '',
+        units: 6,
+        rating: '4.5',
+        description:
+          'Minimal Lamp is made of by natural wood.The design that is very simple and minimal. This is truly one of the best furnutures in any family for now. With 3 different colors, you can easily select the best match for your home.',
+        reviews: [
+          {
+            id: '1',
+            user: 'Bruno Fernandes',
+            rating: '5',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+          {
+            id: '2',
+            user: 'Tracy Mosby',
+            rating: '5',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+          {
+            id: '3',
+            user: 'Busayo Elegbede',
+            rating: '4',
+            review:
+              'Nice furniture with good delivery. The delivery time is very fast. Then product look like exactly the picture in the app. Besides color is also the same and quality is very good despite very cheap price.',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export default products;
