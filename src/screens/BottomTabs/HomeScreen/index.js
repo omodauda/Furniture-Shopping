08fa067f5b57f40c221e7847d4ccfe638e27cd1a
omodauda <omodauda.dl@gmail.com>
2022-07-07 21:17:08 +0100

    feat: home screen header

--- src/screens/BottomTabs/HomeScreen/index.js
+++ src/screens/BottomTabs/HomeScreen/index.js
@@ -1,11 +1,29 @@
 import {View, Text, SafeAreaView} from 'react-native';
 import React from 'react';
 import styles from './styles';
+import COLORS from '@constants/Colors';
+
+// icons
+import Feather from 'react-native-vector-icons/Feather';
+
+const ICON_SIZE = 20;
+const ICON_COLOR = COLORS.gray;
 
 export default function HomeScreen({navigation}) {
   return (
     <SafeAreaView style={styles.screen}>
-      <Text>Home</Text>
+      <View style={styles.header}>
+        <Feather name="search" color={ICON_COLOR} size={ICON_SIZE} />
+        <View>
+          <Text style={[styles.headerText, styles.headerTextThin]}>
+            Make home
+          </Text>
+          <Text style={[styles.headerText, styles.headerTextBold]}>
+            BEAUTIFUL
+          </Text>
+        </View>
+        <Feather name="shopping-cart" color={ICON_COLOR} size={ICON_SIZE} />
+      </View>
     </SafeAreaView>
   );
 }
--- src/screens/BottomTabs/HomeScreen/styles.js
+++ src/screens/BottomTabs/HomeScreen/styles.js
@@ -1,8 +1,31 @@
 import {StyleSheet} from 'react-native';
+import COLORS from '@constants/Colors';
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    backgroundColor: COLORS.white,
+  },
+  header: {
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 5,
+    marginBottom: 20,
+  },
+  headerText: {
+    fontSize: 18,
+    lineHeight: 25,
+    textAlign: 'center',
+  },
+  headerTextThin: {
+    fontFamily: 'Gelasio-Regular',
+    fontWeight: '400',
+  },
+  headerTextBold: {
+    fontFamily: 'Gelasio-Bold',
+    fontWeight: '700',
   },
 });
 
