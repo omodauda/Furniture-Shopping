2b230c8f0641b69d77f0d41b140c47ddd3680b6a
omodauda <omodauda.dl@gmail.com>
2022-07-07 23:05:02 +0100

    style: add color

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -4,6 +4,7 @@ const COLORS = {
   gray2: '#909090',
   gray4: '#BDBDBD',
   gray5: '#E0E0E0',
+  blurGray: '#F0F0F0',
   black: '#303030',
   black3: '#606060',
   shadowColor: 'rgba(138, 149, 158, 0.2)',
