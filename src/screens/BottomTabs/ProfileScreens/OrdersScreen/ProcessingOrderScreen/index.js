c6483e087e6ba54885c12851ab6733b2747cda04
omodauda <omodauda.dl@gmail.com>
2022-07-31 22:14:21 +0100

    feat: Processing Orders Screen

--- src/screens/BottomTabs/ProfileScreens/OrdersScreen/ProcessingOrderScreen/index.js
+++ src/screens/BottomTabs/ProfileScreens/OrdersScreen/ProcessingOrderScreen/index.js
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
+import styles from './styles'
+import USER from '@data/user'
+import OrdersList from '../OrdersList'
 
 export default function ProcessingOrderScreen() {
+  const {orders} = USER;
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.screen}>
+      <OrdersList data={orders} />
     </View>
   )
 }
