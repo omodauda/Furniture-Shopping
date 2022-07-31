117cf4919b39dd35685586f11fe62df880476c54
omodauda <omodauda.dl@gmail.com>
2022-07-31 13:57:17 +0100

    chore: create order screens

---
+++ src/screens/BottomTabs/ProfileScreens/OrdersScreen/MyOrdersScreen/styles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native'
+import COLORS from '@constants/Colors'
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		backgroundColor: COLORS.white
+	}
+})
+
+export default styles
