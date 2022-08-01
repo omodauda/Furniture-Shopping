8f659fc723ba4df92d2f1f8ba25b30291bfd717c
omodauda <omodauda.dl@gmail.com>
2022-08-01 13:00:31 +0100

    feat: Shipping Addresses Screen

--- src/screens/BottomTabs/ProfileScreens/ShippingAddress/index.js
+++ src/screens/BottomTabs/ProfileScreens/ShippingAddress/index.js
@@ -1,12 +1,69 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
-import styles from '../ProfileScreen/styles'
+import { View, Text, SafeAreaView, TouchableOpacity, FlatList } from 'react-native'
+import React, {useState} from 'react'
+import styles from './styles'
 import StackScreenHeader from '@components/StackScreenHeader'
+import COLORS from '@constants/Colors'
+import USER from '@data/user';
+
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
 export default function ShippingAddresses({navigation}) {
+  const {name, shippingAddresses} = USER;
+	const [defaultAddress, setDefaultAddress] = useState('1');
+
+	const handlePress = (id) => {
+		setDefaultAddress(id)
+	}
+
+	const renderItem = ({item}) => {
+		const {address} = item;
+		const isDefaultAddress = item.id === defaultAddress;
+		return (
+			<View style={styles.section}>
+        <View style={styles.sectionHeader}>
+          <TouchableOpacity
+						style={[
+							styles.checkBox,
+							{
+								backgroundColor: isDefaultAddress ? COLORS.black : COLORS.white,
+								borderWidth: isDefaultAddress ? 0 : 1.5,
+								borderColor: isDefaultAddress ? null : COLORS.gray2
+							}
+						]}
+						onPress={() => handlePress(item.id)}
+					>
+						{
+							isDefaultAddress ? <AntDesign name='check' size={20} color={COLORS.white} /> : null
+						}
+          </TouchableOpacity>
+          <Text style={styles.sectionHeaderText}>Use as the shipping address</Text>
+        </View>
+        <View style={styles.card}>
+          <View style={styles.topCard}>
+            <Text style={styles.name}>{name}</Text>
+            <TouchableOpacity>
+                <AntDesign name="edit" size={24} color={COLORS.gray} />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.bottomCard}>
+            <Text style={styles.address}>{address}</Text>
+          </View>
+        </View>
+      </View>
+		)
+	}
   return (
     <SafeAreaView style={styles.screen}>
       <StackScreenHeader navigation={navigation} title="SHIPPING ADDRESS" />
+			<FlatList
+				data={shippingAddresses}
+				keyExtractor={item => item.id}
+				renderItem={renderItem}
+				showsVerticalScrollIndicator={false}
+			/>
+			<TouchableOpacity style={styles.addBtn}>
+				<AntDesign name='plus' size={34} color={COLORS.black} />
+			</TouchableOpacity>
     </SafeAreaView>
   )
 }
--- src/screens/BottomTabs/ProfileScreens/ShippingAddress/styles.js
+++ src/screens/BottomTabs/ProfileScreens/ShippingAddress/styles.js
@@ -4,6 +4,95 @@ import COLORS from '@constants/Colors'
 const styles = StyleSheet.create({
   screen: {
 		flex: 1,
+		backgroundColor: COLORS.white,
+	},
+	section: {
+    paddingHorizontal: 20,
+		marginVertical: 15
+  },
+	sectionHeader: {
+    flexDirection: 'row',
+  },
+  sectionHeaderText: {
+    fontFamily: 'NunitoSans-Regular',
+    fontWeight: '400',
+    fontSize: 18,
+    color: COLORS.gray2,
+		marginLeft: 10
+  },
+	checkBox: {
+		width: 20,
+		height: 20,
+		borderRadius: 4,
+		borderWidth: 1.5,
+	},
+  card: {
+    marginTop: 10,
+		shadowColor: COLORS.shadow,
+		shadowOffset: {
+			width: 0,
+			height: 8
+		},
+		shadowOpacity: 0.5,
+		shadowRadius: 10,
+		elevation: 10
+  },
+  topCard: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+    backgroundColor: COLORS.white,
+    paddingTop: 15,
+    paddingBottom: 10,
+    paddingHorizontal: 20,
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+		borderColor: COLORS.blurGray,
+    borderBottomWidth: 2,
+
+  },
+  name: {
+    fontFamily: 'NunitoSans-Bold',
+    fontWeight: '600',
+    fontSize: 18,
+    color: COLORS.black
+  },
+  bottomCard: {
+    paddingLeft: 20,
+    paddingTop: 10,
+    paddingBottom: 15,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+    borderWidth: 0.5,
+    borderColor: COLORS.blurGray,
+    minHeight: 54,
 		backgroundColor: COLORS.white
+  },
+	address: {
+    fontFamily: 'NunitoSans-Regular',
+    fontWeight: '400',
+    fontSize: 14,
+    color: COLORS.gray,
+    lineHeight: 25,
+  },
+	addBtn: {
+		width: 60,
+		height: 60,
+		borderRadius: 30,
+		backgroundColor: COLORS.white,
+		position: 'absolute',
+		bottom: 35,
+		right: 25,
+		justifyContent: 'center',
+		alignItems: 'center',
+		shadowColor: COLORS.shadow,
+		shadowOffset: {
+			width: 4,
+			height: 4
+		},
+		shadowOpacity: 0.5,
+		shadowRadius: 40,
+		elevation: 10
 	}
 })
+
+export default styles
