90f6822669a6a18f3448f9a9e2fd6258ef7087b8
omodauda <omodauda.dl@gmail.com>
2022-08-05 20:19:12 +0100

    feat: Add Cart Empty View

--- src/screens/BottomTabs/HomeScreens/CartScreen/index.js
+++ src/screens/BottomTabs/HomeScreens/CartScreen/index.js
@@ -9,6 +9,7 @@ import {useSelector} from 'react-redux'
 
 import AntDesign from 'react-native-vector-icons/AntDesign'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
 const {width, height} = Dimensions.get('screen');
 
@@ -16,9 +17,9 @@ const IMG_WIDTH = 0.26 * width;
 const IMG_HEIGHT = 0.12 * height;
 
 export default function CartScreen({ navigation }) {
-  const {products: PRODUCTS, total} = useSelector(state => state.cart)
-  const DATA = PRODUCTS.find(product => product.category === 'Table').list;
-
+  const { products: DATA, total } = useSelector(state => state.cart)
+  // const DATA = PRODUCTS.find(product => product.category === 'Table').list;
+  const isEmptyCart = DATA.length < 1 ? true : false;
   const handleSubmit = () => {
     navigation.navigate('CheckOut');
   }
@@ -45,34 +46,47 @@ export default function CartScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.screen}>
       <StackScreenHeader navigation={navigation} title="MY CART" />
-      <View style={styles.list}>
-        <FlatList
-          data={DATA}
-          keyExtractor={item => item.id}
-          renderItem={renderItem}
-          showsVerticalScrollIndicator={false}
-        />
-      </View>
-      <View style={styles.promo}>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your promo code"
-          placeholderTextColor={COLORS.gray2}
-        />
-        <TouchableOpacity style={styles.iconwrapper}>
-          <Ionicons name="chevron-forward" size={25} color={COLORS.white} />
-        </TouchableOpacity>
-      </View>
-      <View style={styles.total}>
-        <Text style={[styles.totalText, {color: COLORS.gray}]}>Total:</Text>
-        <Text style={[styles.totalText, {color: COLORS.black}]}>$ {total}.00</Text>
-      </View>
-      <CustomButton
-        title="CHECK OUT"
-        btnStyle={styles.btn}
-        titleStyle={styles.btnText}
-        handlePress={handleSubmit}
-      />
+      {
+        isEmptyCart ?
+          <View style={styles.emptyView}>
+            <MaterialIcons name='add-shopping-cart' size={200} color={COLORS.black} />
+            <Text style={styles.emptyTextBold}>Your Cart is Empty</Text>
+            <Text style={styles.emptyDesc}>
+              Looks like you haven't added anything to your cart yet
+            </Text>
+          </View>
+        : 
+        <>
+            <View style={styles.list}>
+              <FlatList
+                data={DATA}
+                keyExtractor={item => item.id}
+                renderItem={renderItem}
+                showsVerticalScrollIndicator={false}
+              />
+            </View>
+            <View style={styles.promo}>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter your promo code"
+                placeholderTextColor={COLORS.gray2}
+              />
+              <TouchableOpacity style={styles.iconwrapper}>
+                <Ionicons name="chevron-forward" size={25} color={COLORS.white} />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.total}>
+              <Text style={[styles.totalText, {color: COLORS.gray}]}>Total:</Text>
+              <Text style={[styles.totalText, {color: COLORS.black}]}>$ {total}.00</Text>
+            </View>
+            <CustomButton
+              title="CHECK OUT"
+              btnStyle={styles.btn}
+              titleStyle={styles.btnText}
+              handlePress={handleSubmit}
+            />
+        </>
+      }
     </SafeAreaView>
   )
 }
