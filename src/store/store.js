47d3fc035bd6082a19939997bf381a33882345c4
omodauda <omodauda.dl@gmail.com>
2022-08-05 16:20:44 +0100

    feat: Add User reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit'
 import productsReducer from './slices/products'
+import userReducer from './slices/user'
 
 export const store = configureStore({
     reducer: {
-			products: productsReducer
+			products: productsReducer,
+			user: userReducer
     }
 })
