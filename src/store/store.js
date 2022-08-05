8fe8a6492a10443fcf57e510c703215469d3e3c9
omodauda <omodauda.dl@gmail.com>
2022-08-05 16:49:01 +0100

    chore: Add Notification reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import productsReducer from './slices/products'
 import userReducer from './slices/user'
+import notificationReducer from './slices/notification'
 
 export const store = configureStore({
-    reducer: {
-			products: productsReducer,
-			user: userReducer
-    }
+  reducer: {
+		products: productsReducer,
+    user: userReducer,
+    notification: notificationReducer
+  }
 })
