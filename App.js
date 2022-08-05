5bdea709e41c8a2483790b3aad288f3dd28e055a
omodauda <omodauda.dl@gmail.com>
2022-08-05 16:03:09 +0100

    feat: Provide redux store to app

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from '@navigations/AppNavigator';
+import {store} from '@store/store'
+import {Provider} from 'react-redux'
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
+    </Provider>
   );
 };
 
