49d792046c325debc7d788729421223f812b15b4
omodauda <omodauda.dl@gmail.com>
2022-08-05 17:02:34 +0100

    feat: Add cart reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import {configureStore} from '@reduxjs/toolkit'
 import productsReducer from './slices/products'
 import userReducer from './slices/user'
 import notificationReducer from './slices/notification'
+import cartReducer from './slices/cart'
 
 export const store = configureStore({
   reducer: {
 		products: productsReducer,
     user: userReducer,
-    notification: notificationReducer
+    notification: notificationReducer,
+    cart: cartReducer
   }
 })
