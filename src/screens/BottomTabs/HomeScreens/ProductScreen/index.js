89698bb9d695c6190c6899841bcc862aef4e0c0a
omodauda <omodauda.dl@gmail.com>
2022-08-06 21:38:41 +0100

    feat: Use AddToCart action

--- src/screens/BottomTabs/HomeScreens/ProductScreen/index.js
+++ src/screens/BottomTabs/HomeScreens/ProductScreen/index.js
@@ -6,12 +6,13 @@ import {
   Image,
   TouchableOpacity,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './styles';
 import COLORS from '@constants/Colors';
 import CustomButton from '@components/CustomButton';
 import CustomUnitControl from '@components/CustomUnitControl';
-import {useSelector} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import {addToCart} from '@store/slices/cart'
 
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Fontisto from 'react-native-vector-icons/Fontisto';
@@ -43,14 +44,32 @@ const PALLETE = [
 ];
 
 export default function ProductScreen({route, navigation}) {
-  const {productId, selectedCategory} = route.params;
+  const { productId, selectedCategory } = route.params;
+  const [qty, setQty] = useState(1);
+
   const PRODUCTS = useSelector(state => state.products)
+  const dispatch = useDispatch()
 
   const item = PRODUCTS.find(
     product => product.category === selectedCategory,
   ).list.find(ware => ware.id === productId);
 
   const isAvailable = item.units > 0 ? true : false;
+
+  const handleIncrease = () => {
+    setQty(qty + 1);
+  }
+
+  const handleDecrease = () => {
+    if (qty > 1) {
+      setQty(qty - 1);
+    }
+    return;
+  }
+
+  const handleAdd = () => {
+    dispatch(addToCart({productId, category: selectedCategory, quantity: qty}))
+  }
 
   return (
     <SafeAreaView>
@@ -100,7 +119,12 @@ export default function ProductScreen({route, navigation}) {
         <Text style={styles.nameText}>{item.name}</Text>
         <View style={styles.row}>
           <Text style={styles.price}>$ {item.price}</Text>
-          <CustomUnitControl style={styles.row} />
+          <CustomUnitControl
+            style={styles.row}
+            qty={qty}
+            handleIncrease={handleIncrease}
+            handleDecrease={handleDecrease}
+          />
         </View>
         <TouchableOpacity
         onPress={() => navigation.navigate('Review', {productId: productId, productCategory: selectedCategory})}
@@ -124,7 +148,7 @@ export default function ProductScreen({route, navigation}) {
           ]}
           titleStyle={styles.buttonText}
           disabled={!isAvailable ? true : false}
-          // handlePress={handleSubmit}
+          handlePress={handleAdd}
         />
       </View>
     </SafeAreaView>
