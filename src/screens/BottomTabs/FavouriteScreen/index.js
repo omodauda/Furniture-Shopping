c890050129dfdc75f3372632a818ccba4aaba375
omodauda <omodauda.dl@gmail.com>
2022-08-08 09:43:10 +0100

    feat: Create Empty State

--- src/screens/BottomTabs/FavouriteScreen/index.js
+++ src/screens/BottomTabs/FavouriteScreen/index.js
@@ -4,10 +4,11 @@ import styles from './styles';
 import TabCustomHeader from '@components/TabCustomHeader';
 import COLORS from '@constants/Colors';
 import CustomButton from '@components/CustomButton';
-import {useSelector} from 'react-redux'
+import { useSelector } from 'react-redux'
 
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Fontisto from 'react-native-vector-icons/Fontisto';
+import Entypo from 'react-native-vector-icons/Entypo'
 
 const {width, height} = Dimensions.get('screen');
 
@@ -15,8 +16,8 @@ const IMG_WIDTH = 0.26 * width;
 const IMG_HEIGHT = 0.12 * height;
 
 export default function FavouriteScreen({ navigation }) {
-  const PRODUCTS = useSelector(state => state.favourite)
-  const favProducts = PRODUCTS.find(product => product.category === 'Popular').list;
+  const favProducts = useSelector(state => state.favourite)
+  const isEmpty = favProducts.length < 1 ? true : false;
 
   const renderItem = ({item}) => (
     <View style={styles.product}>
@@ -33,7 +34,9 @@ export default function FavouriteScreen({ navigation }) {
         <TouchableOpacity>
           <AntDesign name="closecircleo" size={20} color={COLORS.gray4} />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.cartIconWrapper}>
+        <TouchableOpacity
+          style={styles.cartIconWrapper}
+        >
           <Fontisto name="shopping-bag" size={18} color={COLORS.black} />
         </TouchableOpacity>
       </View>
@@ -45,17 +48,31 @@ export default function FavouriteScreen({ navigation }) {
       <TabCustomHeader navigation={navigation}>
         <Text style={styles.headerText}>FAVOURITE</Text>
       </TabCustomHeader>
-      <FlatList
-        data={favProducts}
-        keyExtractor={item => item.id}
-        renderItem={renderItem}
-        contentContainerStyle={styles.list}
-      />
-      <CustomButton
-        title="Add all to my cart"
-        titleStyle={styles.btnText}
-        btnStyle={styles.btn}
-      />
+      {
+        isEmpty ?
+          <View style={styles.emptyView}>
+            <Entypo name='shopping-bag' size={200} color={COLORS.black} />
+            <Text style={styles.emptyTextBold}>Empty Bag</Text>
+            <Text style={styles.emptyDesc}>
+              Looks like you haven't favourited anything yet
+            </Text>
+          </View>
+          :
+          <>
+            <FlatList
+              data={favProducts}
+              keyExtractor={item => item.id}
+              renderItem={renderItem}
+              contentContainerStyle={styles.list}
+            />
+            <CustomButton
+              title="Add all to my cart"
+              titleStyle={styles.btnText}
+              btnStyle={styles.btn}
+            />
+          </>
+      }
+      
     </SafeAreaView>
   );
 }
