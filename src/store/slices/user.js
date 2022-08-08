2f00f21ff2a8a4b57680017f83b322ecb22f039e
omodauda <omodauda.dl@gmail.com>
2022-08-08 20:56:52 +0100

    feat: User signUp reducer

--- src/store/slices/user.js
+++ src/store/slices/user.js
@@ -1,12 +1,29 @@
 import {createSlice} from '@reduxjs/toolkit'
-import USER from '@data/user'
+// import USER from '@data/user'
 
-const initialState = USER;
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  image: require('@assets/images/users/user1.png'),
+  orders: [],
+  shippingAddresses: [],
+  paymentMethods: [],
+  reviews: []
+};
 
 export const userSlice = createSlice({
   name: 'user',
 	initialState,
-	reducers: {}
+  reducers: {
+    signUp: (state, action) => {
+      const { name, email, password } = action.payload;
+      state.name = name;
+      state.email = email;
+      state.password = password;
+    }
+  }
 })
 
+export const {signUp} = userSlice.actions
 export default userSlice.reducer
