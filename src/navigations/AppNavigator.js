cdea4213fed8bbab6618b4e2bd31bc65e7e19e51
omodauda <omodauda.dl@gmail.com>
2022-08-08 20:57:33 +0100

    feat: Add AuthStack to Navigator

--- src/navigations/AppNavigator.js
+++ src/navigations/AppNavigator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 //stacks
-// import AuthStack from '@navigations/AuthStack';
+import AuthStack from '@navigations/AuthStack';
 import BottomTabNavigator from './BottomTabNavigator';
 
 const Stack = createStackNavigator();
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 export default function AppNavigator() {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      {/* <Stack.Screen name="Auth" component={AuthStack} /> */}
+      <Stack.Screen name="Auth" component={AuthStack} />
       <Stack.Screen name="Main" component={BottomTabNavigator} />
     </Stack.Navigator>
   );
