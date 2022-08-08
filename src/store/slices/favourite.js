2187e276e73ef59263c47e9ee0b5c8acb3ebe766
omodauda <omodauda.dl@gmail.com>
2022-08-08 09:38:09 +0100

    feat: AddToFav reducer

--- src/store/slices/favourite.js
+++ src/store/slices/favourite.js
@@ -1,12 +1,32 @@
 import {createSlice} from '@reduxjs/toolkit'
 import PRODUCTS from '@data/products'
 
-const initialState = PRODUCTS
+const initialState = []
 
 export const favouriteSlice = createSlice({
 	name: 'favourite',
 	initialState,
-	reducers: {}
+  reducers: {
+    addToFav: (state, action) => {
+      const { category, productId } = action.payload;
+      const item = PRODUCTS.find(
+        product => product.category === category
+      ).list.find(P => P.id === productId);
+      const { id, name, image, price } = item;
+      const productIndex = state.findIndex(product => product.category === category && product.id === productId)
+      const existingProduct = state[productIndex]
+      if (existingProduct) return;
+      const newItem = {
+        category,
+        id,
+        image,
+        name,
+        price
+      }
+      state.push(newItem)
+    }
+  }
 })
 
+export const {addToFav} = favouriteSlice.actions
 export default favouriteSlice.reducer
