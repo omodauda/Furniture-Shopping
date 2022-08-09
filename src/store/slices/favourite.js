fa3c631517df996d152040f68e6e2a7a461c0cb7
omodauda <omodauda.dl@gmail.com>
2022-08-09 22:27:33 +0100

    feat: Update Favourite Slice

--- src/store/slices/favourite.js
+++ src/store/slices/favourite.js
@@ -1,37 +1,43 @@
-import {createSlice} from '@reduxjs/toolkit'
-import PRODUCTS from '@data/products'
+import {createSlice} from '@reduxjs/toolkit';
+import PRODUCTS from '@data/products';
 
-const initialState = []
+const initialState = {
+  favouriteProducts: [],
+};
 
 export const favouriteSlice = createSlice({
-	name: 'favourite',
-	initialState,
+  name: 'favourite',
+  initialState,
   reducers: {
     addToFav: (state, action) => {
-      const { category, productId } = action.payload;
+      const {category, productId} = action.payload;
       const item = PRODUCTS.find(
-        product => product.category === category
+        product => product.category === category,
       ).list.find(p => p.id === productId);
-      const { id, name, image, price } = item;
-      const productIndex = state.findIndex(product => product.category === category && product.id === productId)
-      const existingProduct = state[productIndex]
+      const {id, name, image, price} = item;
+      const productIndex = state.favouriteProducts.findIndex(
+        product => product.category === category && product.id === productId,
+      );
+      const existingProduct = state[productIndex];
       if (existingProduct) return;
       const newItem = {
         category,
         id,
         image,
         name,
-        price
-      }
-      state.push(newItem)
+        price,
+      };
+      state.favouriteProducts.push(newItem);
     },
     removeFromFav: (state, action) => {
-      const { category, productId } = action.payload;
-      const productIndex = state.findIndex(product => product.category === category && product.id === productId)
-      state.splice(productIndex, 1)
-    }
-  }
-})
+      const {category, productId} = action.payload;
+      const productIndex = state.favouriteProducts.findIndex(
+        product => product.category === category && product.id === productId,
+      );
+      state.splice(productIndex, 1);
+    },
+  },
+});
 
-export const {addToFav, removeFromFav} = favouriteSlice.actions
-export default favouriteSlice.reducer
+export const {addToFav, removeFromFav} = favouriteSlice.actions;
+export default favouriteSlice.reducer;
