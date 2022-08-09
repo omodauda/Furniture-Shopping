1158e1f39419eccda6fedc9d1693cdb3509b854b
omodauda <omodauda.dl@gmail.com>
2022-08-09 22:29:14 +0100

    feat: Update favourite product selector

--- src/screens/BottomTabs/FavouriteScreen/index.js
+++ src/screens/BottomTabs/FavouriteScreen/index.js
@@ -1,34 +1,43 @@
-import {View, Text, SafeAreaView, FlatList, Image, TouchableOpacity, Dimensions} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
 import React from 'react';
 import styles from './styles';
 import TabCustomHeader from '@components/TabCustomHeader';
 import COLORS from '@constants/Colors';
 import CustomButton from '@components/CustomButton';
-import { useSelector, useDispatch } from 'react-redux'
-import {removeFromFav} from '@store/slices/favourite'
+import {useSelector, useDispatch} from 'react-redux';
+import {removeFromFav} from '@store/slices/favourite';
 
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Fontisto from 'react-native-vector-icons/Fontisto';
-import Entypo from 'react-native-vector-icons/Entypo'
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const {width, height} = Dimensions.get('screen');
 
 const IMG_WIDTH = 0.26 * width;
 const IMG_HEIGHT = 0.12 * height;
 
-export default function FavouriteScreen({ navigation }) {
-  const favProducts = useSelector(state => state.favourite)
+export default function FavouriteScreen({navigation}) {
+  const favProducts = useSelector(state => state.favourite.favouriteProducts);
   const isEmpty = favProducts.length < 1 ? true : false;
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const handleRemoveItem = item => {
-    const { category, id } = item;
-    dispatch(removeFromFav({category, productId: id}))
-  }
+    const {category, id} = item;
+    dispatch(removeFromFav({category, productId: id}));
+  };
   const renderItem = ({item}) => (
     <View style={styles.product}>
       <View style={styles.row}>
-        <View style={[styles.imgContainer, {width: IMG_WIDTH, height: IMG_HEIGHT}]}>
+        <View
+          style={[styles.imgContainer, {width: IMG_WIDTH, height: IMG_HEIGHT}]}>
           <Image style={styles.image} source={item.image} resizeMode="cover" />
         </View>
         <View style={styles.details}>
@@ -37,14 +46,10 @@ export default function FavouriteScreen({ navigation }) {
         </View>
       </View>
       <View style={styles.icons}>
-        <TouchableOpacity
-          onPress={() => handleRemoveItem(item)}
-        >
+        <TouchableOpacity onPress={() => handleRemoveItem(item)}>
           <AntDesign name="closecircleo" size={20} color={COLORS.gray4} />
         </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.cartIconWrapper}
-        >
+        <TouchableOpacity style={styles.cartIconWrapper}>
           <Fontisto name="shopping-bag" size={18} color={COLORS.black} />
         </TouchableOpacity>
       </View>
@@ -56,31 +61,29 @@ export default function FavouriteScreen({ navigation }) {
       <TabCustomHeader navigation={navigation}>
         <Text style={styles.headerText}>FAVOURITE</Text>
       </TabCustomHeader>
-      {
-        isEmpty ?
-          <View style={styles.emptyView}>
-            <Entypo name='shopping-bag' size={200} color={COLORS.black} />
-            <Text style={styles.emptyTextBold}>Empty Bag</Text>
-            <Text style={styles.emptyDesc}>
-              Looks like you haven't favourited anything yet
-            </Text>
-          </View>
-          :
-          <>
-            <FlatList
-              data={favProducts}
-              keyExtractor={item => item.id}
-              renderItem={renderItem}
-              contentContainerStyle={styles.list}
-            />
-            <CustomButton
-              title="Add all to my cart"
-              titleStyle={styles.btnText}
-              btnStyle={styles.btn}
-            />
-          </>
-      }
-      
+      {isEmpty ? (
+        <View style={styles.emptyView}>
+          <Entypo name="shopping-bag" size={200} color={COLORS.black} />
+          <Text style={styles.emptyTextBold}>Empty Bag</Text>
+          <Text style={styles.emptyDesc}>
+            Looks like you haven't favourited anything yet
+          </Text>
+        </View>
+      ) : (
+        <>
+          <FlatList
+            data={favProducts}
+            keyExtractor={item => item.id}
+            renderItem={renderItem}
+            contentContainerStyle={styles.list}
+          />
+          <CustomButton
+            title="Add all to my cart"
+            titleStyle={styles.btnText}
+            btnStyle={styles.btn}
+          />
+        </>
+      )}
     </SafeAreaView>
   );
 }
