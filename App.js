77c30834f7222d5f5399e696d2e7d7827425dcc2
omodauda <omodauda.dl@gmail.com>
2022-08-09 22:26:49 +0100

    feat: Provide pesistor to App

--- App.js
+++ App.js
@@ -2,15 +2,18 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from '@navigations/AppNavigator';
-import {store} from '@store/store'
-import {Provider} from 'react-redux'
+import {store, persistor} from '@store/store';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
