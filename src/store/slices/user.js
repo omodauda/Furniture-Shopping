33a107548f5fb96add357e5fce5615a36b4464ef
omodauda <omodauda.dl@gmail.com>
2022-08-12 22:25:03 +0100

    feat: Remove orders from user state

--- src/store/slices/user.js
+++ src/store/slices/user.js
@@ -4,7 +4,6 @@ const initialState = {
   name: null,
   email: null,
   image: require('@assets/images/users/user1.png'),
-  orders: [],
   shippingAddresses: [],
   paymentMethods: [],
   reviews: [],
@@ -42,30 +41,8 @@ export const userSlice = createSlice({
       };
       state.paymentMethods.push(newPaymentMethod);
     },
-    createOrder: (state, action) => {
-      const {deliveryFee, address, orderAmount, quantity, orders} =
-        action.payload;
-
-      const orderNo = Math.floor(100000000 + Math.random() * 900000000);
-      const newOrder = {
-        id: state.orders.length + 1,
-        date: new Date().toLocaleDateString(),
-        status: 'Processing',
-        orderNo,
-        deliveryFee,
-        orderAmount,
-        totalAmount: orderAmount + deliveryFee,
-        quantity,
-        orders,
-        address,
-      };
-      return {
-        orders: [newOrder, ...state.orders],
-      };
-    },
   },
 });
 
-export const {signUp, addShippingAddress, addPaymentMethod, createOrder} =
-  userSlice.actions;
+export const {signUp, addShippingAddress, addPaymentMethod} = userSlice.actions;
 export default userSlice.reducer;
