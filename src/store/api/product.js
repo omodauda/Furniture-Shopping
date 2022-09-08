2db1530bd2cb2930539a4a3c13d7841d6da53622
omodauda <omodauda.dl@gmail.com>
2022-09-08 12:46:24 +0100

    chore: product api

---
+++ src/store/api/product.js
@@ -0,0 +1,37 @@
+import {API_URL } from '@env';
+
+const getCategories = async (name) => {
+  const response = await fetch(`${API_URL}/product/categories?name=${name}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+  if (!response.ok) {
+    const resData = await response.json();
+    console.log('error.response', resData)
+    throw new Error(resData.message);
+  }
+  const resData = await response.json();
+  const products = resData.data.products
+  // console.log('products', resData.data)
+  return products;
+}
+
+const getProductById = async (productId) => {
+  const response = await fetch(`${API_URL}/product/${productId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+  if (!response.ok) {
+    const resData = await response.json();
+    console.log('error.response', resData)
+    throw new Error(resData.message);
+  }
+  const resData = await response.json();
+  return resData.data;
+}
+
+export {getCategories, getProductById}
