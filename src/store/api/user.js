96f2f3e92ee170cac06e2e36ab8bcaa5009b55ff
omodauda <omodauda.dl@gmail.com>
2022-09-08 12:46:37 +0100

    chore: user api

--- src/store/api/user.js
+++ src/store/api/user.js
@@ -20,4 +20,43 @@ const userSignUp = async ({ fullName, email, password }) => {
   return resData;
 }
 
-export {userSignUp}
+const userLogin = async ({ email, password }) => {
+  console.log('api was called')
+  const response = await fetch(`${API_URL}/user/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      password
+    })
+  });
+  if (!response.ok) {
+    const resData = await response.json();
+    console.log('error.response', resData)
+    throw new Error(resData.message);
+  }
+  const resData = await response.json();
+  // console.log('request', resData)
+  return resData;
+}
+
+export {userSignUp, userLogin}
+// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// export const userApi = createApi({
+//   reducerPath: 'userApi',
+//   baseQuery: fetchBaseQuery({ baseUrl: `${API_URL}/user` }),
+//   endpoints: (builder) => ({
+//     login: builder.query({
+//       query: ({ email, password }) => ({
+//         method: 'POST',
+//         body: {email, password}
+//       })
+//     })
+//   })
+// })
+
+
+// export const { useLoginQuery } = userApi;
