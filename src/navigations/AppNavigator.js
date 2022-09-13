1c572e12884020bfe9998b7f22136a6c372b2480
omodauda <omodauda.dl@gmail.com>
2022-09-13 18:34:17 +0100

    feat: Use startup screen in app navigator

--- src/navigations/AppNavigator.js
+++ src/navigations/AppNavigator.js
@@ -1,32 +1,22 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import {QueryCache} from '@tanstack/react-query'
+import { QueryCache } from '@tanstack/react-query';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // import {useSelector} from 'react-redux';
 
 //stacks
 import AuthStack from '@navigations/AuthStack';
 import BottomTabNavigator from './BottomTabNavigator';
+import StartupScreen from '@screens/StartUpScreen';
 
 const Stack = createStackNavigator();
 
-// const queryCache = new QueryCache({
-//   onError: error => {
-//     console.log(error)
-//   },
-//   onSuccess: data => {
-//     console.log(data)
-//   }
-// });
-
-// const query = queryCache.find(['user']);
-// console.log('user_query', query);
-
 export default function AppNavigator() {
-  // const {name, email} = useSelector(state => state.user);
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-        {/* <Stack.Screen name="Auth" component={AuthStack} /> */}
-        <Stack.Screen name="Main" component={BottomTabNavigator} />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name='StartUp' component={StartupScreen} /> 
+      <Stack.Screen name='Auth' component={AuthStack} /> 
+      <Stack.Screen name='Main' component={BottomTabNavigator} />   
     </Stack.Navigator>
   );
 }
