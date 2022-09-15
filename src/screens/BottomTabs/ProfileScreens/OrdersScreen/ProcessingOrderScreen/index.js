756c06d205f9326c00055d0ccf17b1cc164bc2f6
omodauda <omodauda.dl@gmail.com>
2022-09-15 13:45:31 +0100

    feat: use loader component

--- src/screens/BottomTabs/ProfileScreens/OrdersScreen/ProcessingOrderScreen/index.js
+++ src/screens/BottomTabs/ProfileScreens/OrdersScreen/ProcessingOrderScreen/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 import styles from './styles';
 import OrdersList from '../OrdersList';
 import { useQuery } from '@tanstack/react-query';
-import {getOrders } from '@store/api/order';
+import { getOrders } from '@store/api/order';
+import Loader from '@components/Loader';
 
 export default function ProcessingOrderScreen() {
   const orderStatus = 'Processing';
@@ -22,7 +23,7 @@ export default function ProcessingOrderScreen() {
   }
 
   if (isLoading) {
-    return <Text>Loading....</Text>
+    return <Loader />
   }
   return (
     <View style={styles.screen}>
