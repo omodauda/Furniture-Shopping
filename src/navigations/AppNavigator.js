fffb079b41d927125c0f11c243ec0acabc4bd2f7
omodauda <omodauda.dl@gmail.com>
2022-10-09 10:39:22 +0100

    chore: Install react-native-flash-message

--- src/navigations/AppNavigator.js
+++ src/navigations/AppNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { QueryCache } from '@tanstack/react-query';
+import {createStackNavigator} from '@react-navigation/stack';
+import {QueryCache} from '@tanstack/react-query';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 // import {useSelector} from 'react-redux';
 
@@ -13,10 +13,10 @@ const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='StartUp' component={StartupScreen} /> 
-      <Stack.Screen name='Auth' component={AuthStack} /> 
-      <Stack.Screen name='Main' component={BottomTabNavigator} />   
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="StartUp" component={StartupScreen} />
+      <Stack.Screen name="Auth" component={AuthStack} />
+      <Stack.Screen name="Main" component={BottomTabNavigator} />
     </Stack.Navigator>
   );
 }
