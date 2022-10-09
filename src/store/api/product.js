9b0c5c292c50022f3622f051c46e5644299c3779
omodauda <omodauda.dl@gmail.com>
2022-10-09 12:39:38 +0100

    chore: use request time out on product api

--- src/store/api/product.js
+++ src/store/api/product.js
@@ -1,37 +1,42 @@
-import { API_URL } from '@constants/url';
+import {API_URL} from '@constants/url';
+import {requestTimeout} from '@utils/request';
 
-const getCategories = async (name) => {
-  const response = await fetch(`${API_URL}/product/categories?name=${name}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  });
+const getCategories = async name => {
+  const response = await requestTimeout(
+    fetch(`${API_URL}/product/categories?name=${name}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }),
+  );
   if (!response.ok) {
     const resData = await response.json();
-    console.log('error.response', resData)
+    console.log('error.response', resData);
     throw new Error(resData.message);
   }
   const resData = await response.json();
-  const products = resData.data.products
+  const products = resData.data.products;
   // console.log('products', resData.data)
   return products;
-}
+};
 
-const getProductById = async (productId) => {
-  const response = await fetch(`${API_URL}/product/${productId}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  });
+const getProductById = async productId => {
+  const response = await requestTimeout(
+    fetch(`${API_URL}/product/${productId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }),
+  );
   if (!response.ok) {
     const resData = await response.json();
-    console.log('error.response', resData)
+    console.log('error.response', resData);
     throw new Error(resData.message);
   }
   const resData = await response.json();
   return resData.data;
-}
+};
 
-export {getCategories, getProductById}
+export {getCategories, getProductById};
